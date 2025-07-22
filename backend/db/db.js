9e71f8703f2c7b5cfe1baabd588a28cfc5056ba3
Prerit-Bhagat <preritbhagat001@gmail.com>
2025-07-22 23:22:19 +0530

    db link updated

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,28 +1,3 @@
-// // db.js
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-
-// // Load environment variables
-// dotenv.config();
-
-// const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
-// const DB_NAME = "CaerWebsite";
-
-// const ConnectDb = async () => {
-//   try {
-//     await mongoose.connect(`${MONGODB_CONNECTION_URL}/${DB_NAME}`, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-
-//     console.log("MongoDB Connected!");
-//   } catch (err) {
-//     console.error("MongoDB Connection Error:", err);
-//     process.exit(1);
-//   }
-// };
-
-// export { ConnectDb };
 // db.js
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -30,12 +5,17 @@ import dotenv from "dotenv";
 // Load environment variables
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGO_URI;
+const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+const DB_NAME = "CarWebsite";
 
 const ConnectDb = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
-    console.log("MongoDB Connected Successfully!");
+    await mongoose.connect(`${MONGODB_CONNECTION_URL}/${DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected!");
   } catch (err) {
     console.error("MongoDB Connection Error:", err);
     process.exit(1);
