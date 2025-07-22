d60af39bbea86ed4fc8012269bf8eae1a4bd4afc
Prerit-Bhagat <preritbhagat001@gmail.com>
2025-07-22 20:25:02 +0530

    db added

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,3 +1,28 @@
+// // db.js
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// // Load environment variables
+// dotenv.config();
+
+// const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+// const DB_NAME = "CaerWebsite";
+
+// const ConnectDb = async () => {
+//   try {
+//     await mongoose.connect(`${MONGODB_CONNECTION_URL}/${DB_NAME}`, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+
+//     console.log("MongoDB Connected!");
+//   } catch (err) {
+//     console.error("MongoDB Connection Error:", err);
+//     process.exit(1);
+//   }
+// };
+
+// export { ConnectDb };
 // db.js
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -9,10 +34,7 @@ const MONGODB_URI = process.env.MONGO_URI;
 
 const ConnectDb = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log("MongoDB Connected Successfully!");
   } catch (err) {
     console.error("MongoDB Connection Error:", err);
